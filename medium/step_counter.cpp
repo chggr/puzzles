6e04548adf852b8780b32f3a6d8b18eb94ac1db1
Christos Gkekas <chris.gekas@gmail.com>
2017-09-30 20:01:40 +0100

    Adding second solution to step_counter.cpp

--- medium/step_counter.cpp
+++ medium/step_counter.cpp
@@ -9,42 +9,60 @@
 //      If there are 4 steps, the answer is 7: 1111 211 121 112 22 31 13
 //      If there are 5 steps, the answer is 13: 11111 2111 1211 1121 1112 221
 //                                              212 122 311 131 113 32 23
+//
+// Solution: The first implementation below is a simple recursive approach.
+// This approach is not very efficient and its runtime complexity is O(n^3),
+// because each recursive call branches out to three more calls.
+//
+// The second implementation uses memoization to store intermediate results
+// so that the same calculations do not need to be repeated multiple times.
+// Both the runtime and space complexities of this implementation are O(n).
 
 int count(int steps) {
-    if (steps > 3) {
-        return count(steps-3) + count(steps-2) + count(steps-1);
-    } else if (steps == 3) {
-        return 1 + count(steps-2) + count(steps-1);
-    } else if (steps == 2) {
-        return 1 + count(steps-1);
-    } else if (steps == 1) {
-        return 1;
+    if (steps < 0) return 0;
+    if (steps == 0) return 1;
+    return count(steps - 1) + count (steps - 2) + count(steps - 3);
+}
+
+int count(int steps, int *mem) {
+    if (steps < 0) return 0;
+    if (steps == 0) return 1;
+
+    if (mem[steps] == 0) {
+        mem[steps] = count(steps - 1, mem) +
+                     count(steps - 2, mem) +
+                     count(steps - 3, mem);
     }
-    return 0;
+    return mem[steps];
+}
+
+int count_mem(int steps) {
+    int mem[steps + 1] = { 0 };
+    return count(steps, mem);
 }
 
 bool test_zero_steps() {
-    return count(0) == 0;
+    return count(0) == 1 && count_mem(0) == 1;
 }
 
 bool test_one_step() {
-    return count(1) == 1;
+    return count(1) == 1 && count_mem(1) == 1;
 }
 
 bool test_two_steps() {
-    return count(2) == 2;
+    return count(2) == 2 && count_mem(2) == 2;
 }
 
 bool test_three_steps() {
-    return count(3) == 4;
+    return count(3) == 4 && count_mem(3) == 4;
 }
 
 bool test_four_steps() {
-    return count(4) == 7;
+    return count(4) == 7 && count_mem(4) == 7;
 }
 
 bool test_five_steps() {
-    return count(5) == 13;
+    return count(5) == 13 && count_mem(5) == 13;
 }
 
 int main() {
