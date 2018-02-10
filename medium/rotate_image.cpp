17efdd40c77bb5de00a2576fb42ef9a3deed7f21
Christos Gkekas <chris.gekas@gmail.com>
2018-02-10 12:56:20 +0000

    Improve readability in rotate_image.cpp

--- medium/rotate_image.cpp
+++ medium/rotate_image.cpp
@@ -45,22 +45,46 @@ bool are_equal(int* a, int* b, int n) {
 }
 
 bool test_3x3_array() {
-    int input[] = {1,2,3,4,5,6,7,8,9};
-    int expected[] = {3,6,9,2,5,8,1,4,7};
+    int input[] = {1, 2, 3,
+                   4, 5, 6,
+                   7, 8, 9};
+
+    int expected[] = {3, 6, 9,
+                      2, 5, 8,
+                      1, 4, 7};
+
     rotate(input, 3);
-    return are_equal(input, expected, 3); 
+    return are_equal(input, expected, 3);
 }
 
 bool test_4x4_array() {
-    int input[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
-    int expected[] = {4,8,12,16,3,7,11,15,2,6,10,14,1,5,9,13};
+    int input[] = {1,  2,  3,  4,
+                   5,  6,  7,  8,
+                   9,  10, 11, 12,
+                   13, 14, 15, 16};
+
+    int expected[] = {4, 8, 12, 16,
+                      3, 7, 11, 15,
+                      2, 6, 10, 14,
+                      1, 5,  9, 13};
+
     rotate(input, 4);
     return are_equal(input, expected, 4);
 }
 
 bool test_5x5_array() {
-    int input[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
-    int expected[] = {5,10,15,20,25,4,9,14,19,24,3,8,13,18,23,2,7,12,17,22,1,6,11,16,21};
+    int input[] = {1,  2,  3,  4,  5,
+                   6,  7,  8,  9,  10,
+                   11, 12, 13, 14, 15,
+                   16, 17, 18, 19, 20,
+                   21, 22, 23, 24, 25};
+
+    int expected[] = {5, 10, 15, 20, 25,
+                      4,  9, 14, 19, 24,
+                      3,  8, 13, 18, 23,
+                      2,  7, 12, 17, 22,
+                      1,  6, 11, 16, 21};
+
     rotate(input, 5);
     return are_equal(input, expected, 5);
 }
