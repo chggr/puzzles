d29b6d170db6bcd3523a18d0c03965e301c2bbdd
Christos Gkekas <chris.gekas@gmail.com>
2018-03-04 15:01:15 +0000

    Adding second implementation to check_tree_balanced.cpp

--- easy/check_tree_balanced.cpp
+++ easy/check_tree_balanced.cpp
@@ -3,8 +3,28 @@
 
 // Task description: Given the root of a binary tree, write a method to
 // determine whether the tree is balanced. A balanced binary tree is a tree
-// such that the heights of the two subtrees under any node never differ by
+// such that the heights of the two sub-trees under any node never differ by
 // more than one.
+//
+// E.g. the first tree below is balanced whereas the second is not.
+//
+//          5                     5
+//       2     7                2
+//     3   4     9            3
+//
+// Solution: The first solution below recursively checks for every node in the
+// tree that its left and right sub-trees have the same height. The runtime
+// complexity of this implementation is O(N * logN) because each node is touched
+// once per node above it. It is not very efficient, since method height() is
+// called repeatedly on the same nodes.
+//
+// The second implementation combines the operation of getting the height of a
+// node with checking whether it is balanced. If the node is not balanced, error
+// code -1 is returned which signals that the whole tree is not balanced. This
+// code is propagated immediately at the top, thus saving further calls to
+// height(). The runtime complexity of this approach is O(N) since each node is
+// touched only once. The space complexity is O(D) where D is the depth of the
+// tree. This space is required for the recursive stack.
 
 class Node {
 
@@ -36,10 +56,30 @@ bool is_balanced(Node* node) {
            is_balanced(node->getRight());
 }
 
+int height2(Node* node) {
+    if (!node) return 0;
+
+    int height_left = height2(node->getLeft());
+    if (height_left == -1) return -1;
+
+    int height_right = height2(node->getRight());
+    if (height_right == -1) return -1;
+
+    if (abs(height_left - height_right) > 1) return -1;
+    return height_left > height_right ?
+        height_left + 1 : height_right + 1;
+}
+
+bool is_balanced2(Node* node) {
+    if (!node) return true;
+    return height2(node) != -1;
+}
+
 bool test_tree_one_node() {
     Node root(1, 0, 0);
 
-    return is_balanced(&root);
+    return is_balanced(&root) &&
+           is_balanced2(&root);
 }
 
 bool test_tree_balanced() {
@@ -48,7 +88,8 @@ bool test_tree_balanced() {
     Node right(3, 0, 0);
     Node root(4, &left, &right);
 
-    return is_balanced(&root);
+    return is_balanced(&root) &&
+           is_balanced2(&root);
 }
 
 bool test_tree_not_balanced() {
@@ -57,7 +98,8 @@ bool test_tree_not_balanced() {
     Node left(3, &leftleft, 0);
     Node root(4, &left, 0);
 
-    return !is_balanced(&root);
+    return !is_balanced(&root) &&
+           !is_balanced2(&root);
 }
 
 int main() {
