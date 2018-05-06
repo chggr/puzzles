84acd1e86dc83afc9f8cd1f4b4fad8fc0e5c2a34
Christos Gkekas <chris.gekas@gmail.com>
2018-05-06 18:51:10 +0100

    Fix whitespace in remove_duplicate_chars.c

--- easy/remove_duplicate_chars.c
+++ easy/remove_duplicate_chars.c
@@ -11,62 +11,62 @@
 //      For input string abbcc\0, the method should return abc\0.
 
 void remove_duplicates(char *input) {
-	if (!input) return;
+    if (!input) return;
 
-	char *current = input;
-	char *last = input;
-	while (*(++current)) {
-		char *temp = input;
-		while (temp <= last && *temp != *current) temp++;
-		if (temp == last + 1) {
-			*(++last) = *current;
-		}
-	}
-	*(++last) = 0;
+    char *current = input;
+    char *last = input;
+    while (*(++current)) {
+        char *temp = input;
+        while (temp <= last && *temp != *current) temp++;
+        if (temp == last + 1) {
+            *(++last) = *current;
+        }
+    }
+    *(++last) = 0;
 }
 
 int test_empty_input() {
-	char input[] = "";
-	remove_duplicates(input);
-	return strcmp(input, "") == 0;
+    char input[] = "";
+    remove_duplicates(input);
+    return strcmp(input, "") == 0;
 }
 
 int test_no_duplicates() {
-	char input[] = "abc";
-	remove_duplicates(input);
-	return strcmp(input, "abc") == 0;
+    char input[] = "abc";
+    remove_duplicates(input);
+    return strcmp(input, "abc") == 0;
 }
 
 int test_with_duplicates() {
-	char input[] = "aabbcc";
-	remove_duplicates(input);
-	return strcmp(input, "abc") == 0;
+    char input[] = "aabbcc";
+    remove_duplicates(input);
+    return strcmp(input, "abc") == 0;
 }
 
 int test_only_duplicates() {
-	char input[] = "aaaa";
-	remove_duplicates(input);
-	return strcmp(input, "a") == 0;
+    char input[] = "aaaa";
+    remove_duplicates(input);
+    return strcmp(input, "a") == 0;
 }
 
 void main() {
-	int counter = 0;
-	if (!test_empty_input()) {
-		printf("Empty input test failed!\n");
-		counter++;
-	}
-	if (!test_no_duplicates()) {
-		printf("No duplicates test failed!\n");
-		counter++;
-	}
-	if (!test_with_duplicates()) {
-		printf("With duplicates test failed!\n");
-		counter++;
-	}
-	if (!test_only_duplicates()) {
-		printf("Only duplicates test failed!\n");
-		counter++;
-	}
-	printf("%d tests failed!\n", counter);
+    int counter = 0;
+    if (!test_empty_input()) {
+        printf("Empty input test failed!\n");
+        counter++;
+    }
+    if (!test_no_duplicates()) {
+        printf("No duplicates test failed!\n");
+        counter++;
+    }
+    if (!test_with_duplicates()) {
+        printf("With duplicates test failed!\n");
+        counter++;
+    }
+    if (!test_only_duplicates()) {
+        printf("Only duplicates test failed!\n");
+        counter++;
+    }
+    printf("%d tests failed!\n", counter);
 }
 
