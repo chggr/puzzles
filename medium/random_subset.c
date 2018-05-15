b73b0916e9369874487b5c559f8c14a09ebb71e8
Christos Gkekas <chris.gekas@gmail.com>
2018-05-15 14:34:05 +0100

    Adding proof to puzzle random_subset.c

--- medium/random_subset.c
+++ medium/random_subset.c
@@ -13,6 +13,25 @@
 // than m, then that position in the target array is set to be equal to the
 // element in the original array. This algorithm guarantees that each number in
 // the original array has equal probability of being chosen.
+//
+// Probability of last element being included: This is equal to the probability
+// of randomly picking a number between 0 and n that is also less than m, which
+// is m / n.
+//
+// Probability of second to last element being included: This can be calculated
+// as [probability of choosing a number between 0 and n - 1 that is also less
+// than m] * [probability that the item will not be replaced by the last item
+// in the array] = [m / (n - 1)] * [(n - 1) / n] = m / n. We can follow this
+// logic to prove that the probability of any element array[i] being included
+// is m / n, where m < i <= n.
+//
+// Probability of any element array[i] where 0 <= i < m being included: This
+// can be calculated as the probability that the item was NOT swapped out when
+// considering elements array[i] where m <= i < n. This is equal to
+// [m / (m + 1)] * [(m + 1) / (m + 2)] * ... * [(n - 1) / n] = m / n
+//
+// Therefore we have proven that the probability to pick each item in the array
+// is equal to m / n for all items.
 
 void subset(int a[], int a_size, int b[], int b_size) {
     if (a_size < b_size) return;
