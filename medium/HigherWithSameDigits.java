bbd974a59bb8cd6ec5359e17fa3bee645abc81e4
Christos Gkekas <chris.gekas@gmail.com>
2018-05-17 11:23:45 +0100

    Adding second solution to HigherWithSameDigits.java

--- medium/HigherWithSameDigits.java
+++ medium/HigherWithSameDigits.java
@@ -18,16 +18,26 @@ import java.util.Arrays;
 // same histogram. The runtime complexity of this approach is O(N * 10^N), where
 // N is the number of digits of the given integer. The algorithm potentially
 // examines 10^N candidates and for each one extracts and compares its digits.
+//
+// The second implementation is much more efficient and can compute the next
+// higher integer is O(N * logN). Initially we extract all digits of the input
+// integer into an array and traverse the array from right to left to identify
+// the position of the first digit D that is smaller than the previously
+// traversed. We then search the right side for the smallest digit M that is
+// also smaller than D. We then swap these two digits and sort all digits on
+// the right side to arrive to the final result. The runtime complexity of this
+// algorithm is O(N * logN) where N is the number of digits in the input integer
+// because of the sorting required at the final step.
 
 public class HigherWithSameDigits {
 
-    private static int[] getDigitHist(int number) {
-        int[] digits = new int[10];
+    private static int[] getHist(int number) {
+        int[] hist = new int[10];
         while (number > 0) {
-            digits[number % 10]++;
+            hist[number % 10]++;
             number /= 10;
         }
-        return digits;
+        return hist;
     }
 
     private static int getDigitCount(int number) {
@@ -42,83 +52,162 @@ public class HigherWithSameDigits {
     private static int find(int number) {
         if (number <= 0) return 0;
 
-        int[] hist = getDigitHist(number);
+        int[] hist = getHist(number);
         int count = getDigitCount(number);
 
         for (int i = number + 1; i < Math.pow(10, count + 1); i++) {
-            if (Arrays.equals(hist, getDigitHist(i))) return i;
+            if (Arrays.equals(hist, getHist(i))) return i;
         }
         return number;
     }
 
-    private static boolean testGetDigitHist() {
-        int[] expected = {1, 1, 1, 0, 1, 2, 0, 0, 1, 1};
-        return Arrays.equals(expected, getDigitHist(25051984));
+    private static int[] getDigits(int number) {
+        int count = getDigitCount(number);
+        int[] digits = new int[count];
+        for (int i = count - 1; i >= 0; i--) {
+            digits[i] = number % 10;
+            number /= 10;
+        }
+        return digits;
+    }
+
+    private static int getNumber(int[] digits) {
+        int result = 0;
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] < 0) return 0;
+            result *= 10;
+            result += digits[i];
+        }
+        return result;
+    }
+
+    private static void swap(int[] array, int a, int b) {
+        if (a == b) return;
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+
+    private static int find2(int number) {
+        if (number <= 0) return 0;
+        int[] digits = getDigits(number);
+
+        // Find the position where current digit D is smaller than previous digit.
+        int pos = digits.length - 2;
+        while (pos >= 0 && digits[pos] >= digits[pos + 1]) pos--;
+        if (pos < 0) return number;
+
+        // Search on the right side to find the smallest digit M that is
+        // also greater than digit D.
+        int min = Integer.MAX_VALUE;
+        int minIndex = pos;
+        for (int i = minIndex + 1; i < digits.length; i++) {
+            if (digits[i] > digits[pos] && digits[i] < min) {
+                min = digits[i];
+                minIndex = i;
+            }
+        }
+        if (minIndex == pos) return number;
+
+        // Swap digits D with M.
+        swap(digits, pos, minIndex);
+
+        // Sort all remaining digits on the right and return the result.
+        Arrays.sort(digits, pos + 1, digits.length);
+        return getNumber(digits);
+    }
+
+    private static boolean testGetHist() {
+        return
+            Arrays.equals(new int[] {1, 1, 1, 1, 0, 0, 0, 0, 0, 0}, getHist(1230)) &&
+            Arrays.equals(new int[] {0, 0, 0, 0, 1, 1, 1, 0, 0, 0}, getHist(546)) &&
+            Arrays.equals(new int[] {0, 0, 0, 0, 0, 0, 0, 1, 1, 1}, getHist(978)) &&
+            Arrays.equals(new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, getHist(0)) &&
+            Arrays.equals(new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, getHist(-1));
     }
 
     private static boolean testGetDigitCount() {
-        return 0 == getDigitCount(0) &&
-               1 == getDigitCount(4) &&
+        return 1 == getDigitCount(4) &&
                3 == getDigitCount(528) &&
-               8 == getDigitCount(25051984);
+               8 == getDigitCount(29504518) &&
+               0 == getDigitCount(0) &&
+               0 == getDigitCount(-1);
     }
 
-    private static boolean testZero() {
-        return 0 == find(0);
+    private static boolean testGetDigits() {
+        return Arrays.equals(new int[] {7}, getDigits(7)) && 
+               Arrays.equals(new int[] {3, 8, 2}, getDigits(382)) &&
+               Arrays.equals(new int[] {9, 6, 2, 8, 1}, getDigits(96281)) &&
+               Arrays.equals(new int[] {}, getDigits(0)) &&
+               Arrays.equals(new int[] {}, getDigits(-1));
     }
 
-    private static boolean testNegative() {
-        return 0 == find (-10);
+    private static boolean testGetNumber() {
+        return 0 == getNumber(new int[] {}) &&
+               0 == getNumber(new int[] {-1}) &&
+               7 == getNumber(new int[] {7}) &&
+               396 == getNumber(new int[] {3, 9, 6});
     }
 
-    private static boolean testOneDigit() {
-        return 8 == find(8);
+    private static boolean testFind() {
+        return 0 == find(0) &&
+               0 == find(-10) &&
+               8 == find(8) &&
+               1243 == find(1234) &&
+               9876 == find(9876) &&
+               9999 == find(9999) &&
+               201 == find(120) &&
+               251678 == find(218765);
     }
 
-    private static boolean testHigherExists() {
-        return 1243 == find(1234);
+    private static boolean testFind2() {
+        return 0 == find2(0) &&
+               0 == find2(-10) &&
+               8 == find2(8) &&
+               1243 == find2(1234) &&
+               9876 == find2(9876) &&
+               9999 == find2(9999) &&
+               201 == find2(120) &&
+               251678 == find2(218765);
     }
 
-    private static boolean testHigherNotExists() {
-        return 98765 == find(98765);
-    }
-
-    private static boolean testDoubleDigits() {
-        return 6995 == find(6959);
+    private static boolean testFindBoth() {
+        for (int i = 0; i < 10000; i++) {
+            if (find(i) != find2(i)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
         int counter = 0;
-        if (!testGetDigitHist()) {
-            System.out.println("Get digit hist test failed!");
+        if (!testGetHist()) {
+            System.out.println("Get hist test failed!");
             counter++;
         }
         if (!testGetDigitCount()) {
             System.out.println("Get digit count test failed!");
             counter++;
         }
-        if (!testZero()) {
-            System.out.println("Zero test failed!");
+        if (!testGetDigits()) {
+            System.out.println("Get digits test failed!");
             counter++;
         }
-        if (!testNegative()) {
-            System.out.println("Negative test failed!");
+        if (!testGetNumber()) {
+            System.out.println("Get number test failed!");
             counter++;
         }
-        if (!testOneDigit()) {
-            System.out.println("One digit test failed!");
+        if (!testFind()) {
+            System.out.println("Find test failed!");
             counter++;
         }
-        if (!testHigherExists()) {
-            System.out.println("Higher exists test failed!");
+        if (!testFind2()) {
+            System.out.println("Find2 test failed!");
             counter++;
         }
-        if (!testHigherNotExists()) {
-            System.out.println("Higher not exists test failed!");
-            counter++;
-        }
-        if (!testDoubleDigits()) {
-            System.out.println("Double digits test failed!");
+        if (!testFindBoth()) {
+            System.out.println("Find both test failed!");
             counter++;
         }
         System.out.println(counter + " tests failed");
